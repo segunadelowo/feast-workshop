67c9daf4b4330e2d28b12ba2aaffd2a44f6745d9
Segun Adelowo <adelowo@live.com>
2023-06-19 17:55:45 +0100

    feat: Add field

--- module_0/feature_repo_aws/features.py
+++ module_0/feature_repo_aws/features.py
@@ -17,9 +17,10 @@ driver_hourly_stats_view = FeatureView(
     schema=[
         Field(name="conv_rate", dtype=Float32),
         Field(name="acc_rate", dtype=Float32),
+        Field(name="miles_driven", dtype=Float32),
     ],
     online=True,
     source=driver_stats,
     tags={"production": "True"},
-    owner="ade@gmail.com",
+    owner="adelowo5@gmail.com",
 )
